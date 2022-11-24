8c74f1a0af795d9ace1f9a471d79ac7a59140b71
ggooltae <waterboy94@naver.com>
2022-11-24 16:43:44 +0900

    feat: firebase 로그인 frontend 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import { getRedirectResult } from "firebase/auth";
 
-function App() {
+import Logo from "./assets/images/responsiview-logo.png";
+import { provider, auth, googleSignIn } from "./service/firebase";
+
+import { COLOR } from "./config/constants";
+
+export default function App() {
+  useEffect(() => {
+    (async () => {
+      try {
+        const result = await getRedirectResult(auth);
+        if (result) {
+          console.log(result);
+          window.location.replace(
+            `responsiview://result=success/userEmail=${result.user.email}`,
+          );
+        } else {
+          googleSignIn(auth, provider);
+        }
+      } catch (error) {
+        window.location.replace(
+          `responsiview://result=fail/code=${error.code}/message=${error.message}`,
+        );
+      }
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Global />
+      <Container>
+        <Image src={Logo}></Image>
+        <Text>Responsiview 앱으로 이동 중입니다.</Text>
+      </Container>
+    </>
   );
 }
 
-export default App;
+const Global = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    user-select: none;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${COLOR.LIGHT_GRAY};
+`;
+
+const Image = styled.img`
+  width: 10rem;
+  height: 10rem;
+  margin-bottom: 2rem;
+`;
+
+const Text = styled.p`
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
---
+++ src/config/constants.js
@@ -0,0 +1,14 @@
+export const COLOR = {
+  WHITE: "#FFFFFF",
+  IVORY: "#F9F7F7",
+  BEIGE: "#EAE3D2",
+  LIGHT_GRAY: "#DBE2EF",
+  GRAY: "#424242",
+  DARK_GRAY: "#2B2B2B",
+  NAVY_GRAY: "#404258",
+  DARK_NAVY: "#112D4E",
+  DARK_BLUE: "#3F72AF",
+  RED: "#F55858",
+  DARK_RED: "#c70e00",
+  LIGHT_GREEN: "#82CA9D",
+};
